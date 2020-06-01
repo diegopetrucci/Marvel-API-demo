d9f1d5c5df473721311589b6475a4b29ea4c650d
diegopetrucci <baulei@gmail.com>
2020-06-01 20:49:47 +0100

    Create Root View: header, character list & cell

--- plum-demo/plum-demo/ContentView.swift
+++ plum-demo/plum-demo/ContentView.swift
@@ -1,21 +1,29 @@
-//
-//  ContentView.swift
-//  plum-demo
-//
-//  Created by Diego Petrucci on 01/06/2020.
-//  Copyright © 2020 Diego Petrucci. All rights reserved.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    var superheroes: [Superhero]
+
     var body: some View {
-        Text("Hello, World!")
+        RootView(superheroes: superheroes)
+            .background(Color(red: 34 / 255, green: 37 / 255, blue: 43 / 255))
+            // Ignoring the bottom safe area to make sure
+            // the background color applies to that as well.
+            .edgesIgnoringSafeArea(.bottom)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(
+            superheroes: [
+                Superhero.fixture(),
+                Superhero.fixture(),
+                Superhero.fixture(),
+                Superhero.fixture(),
+                Superhero.fixture(),
+                Superhero.fixture(),
+                Superhero.fixture()
+            ]
+        )
     }
 }

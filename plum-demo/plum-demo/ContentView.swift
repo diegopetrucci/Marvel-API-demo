1418ea7aec6423992a2bb416383259b3215dde0a
diegopetrucci <baulei@gmail.com>
2020-06-01 18:28:08 +0100

    Initialize Xcode project

---
+++ plum-demo/plum-demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  plum-demo
+//
+//  Created by Diego Petrucci on 01/06/2020.
+//  Copyright © 2020 Diego Petrucci. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

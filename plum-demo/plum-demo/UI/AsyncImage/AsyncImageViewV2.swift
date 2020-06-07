58c0757e41ddf3294a934760b8726f4964d63d74
diegopetrucci <baulei@gmail.com>
2020-06-07 17:14:09 +0100

    Explain what the purpose of AsyncImage*V2 is

--- plum-demo/plum-demo/UI/AsyncImage/AsyncImageViewV2.swift
+++ plum-demo/plum-demo/UI/AsyncImage/AsyncImageViewV2.swift
@@ -1,12 +1,18 @@
 import SwiftUI
 
+// To see what was up with the images in the `my squad` view,
+// I've tried another approach to download them. Even
+// with this one they seem to disappear when coming back
+// from the detail view. At this point I believe it's a
+// SwiftUI bug, or at the very least a quick of its layout
+// system that is not fully clear yet.
 struct AsyncImageViewV2: View {
     @ObservedObject var viewModel: AsyncImageViewModelV2
     var contentMode: ContentMode = .fit
 
     var body: some View {
         image()
-            .onAppear(perform: viewModel.fetch())
+            .onAppear(perform: viewModel.fetch)
             .onDisappear(perform: viewModel.cancel)
     }
 }
